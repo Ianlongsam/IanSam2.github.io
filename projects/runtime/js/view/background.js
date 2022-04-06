8f3acf76d85d65e4b843faa534cacdabc84f29b4
Ianlongsam <l.sam@ardencahillacademy.com>
2022-04-06 16:44:55 +0000

    why again

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -66,7 +66,7 @@ var background = function (window) {
 
             // TODO 4: Part 1 - Add a tree
             tree = draw.bitmap('img/tree.png');
-            tree.x = 1g0;
+            tree.x = 10;
             tree.y = 40;
             background.addChild(tree);
 
